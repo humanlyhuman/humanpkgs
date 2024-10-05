ec5375c14a94ddc6a01c4c1b7b21e4150dfa9204
humanlyhuman <humanlyhuman420@pm.me>
2024-10-05 17:22:07 +0200

    new file:   pkgs/by-name/biglybt/package.nix 	modified:   pkgs/biglybt/default.nix 	modified:   pkgs/biglybt/result 	deleted:    pkgs/torzu/default.nix

---
+++ pkgs/by-name/biglybt/package.nix
@@ -0,0 +1,30 @@
+# hello.nix
+{
+  stdenv,
+  fetchzip,
+  swt,
+  jre,
+  gtk3,
+}:
+
+stdenv.mkDerivation {
+  pname = "biglybt";
+  version = "3.7.0.0";
+
+  buildInputs = [ 
+    jre
+    swt
+    gtk3
+  ];
+
+  src = fetchzip {
+    url = "https://github.com/BiglySoftware/BiglyBT/releases/download/v3.7.0.0/GitHub_BiglyBT_unix.tar.gz";
+    sha256 = "sha256-pjoaDO0cnhVMK1EckrWna6gB5du5ZxgySvxKrbB2N4o=";
+  };
+  buildPhase = ''
+    '';         
+  installPhase = ''
+    mkdir -p $out/bin
+    cp -r ./* $out/bin 
+  '';
+}
